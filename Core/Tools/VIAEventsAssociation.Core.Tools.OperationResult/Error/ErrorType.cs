f8d93ac837d50f46cf23df372c5a21f9548fa921
banny99 <benjamin.bartosik@gmail.com>
2024-03-02 19:36:11 +0100

    (assignment-4): implement UC5-UC9

--- Core/Tools/VIAEventsAssociation.Core.Tools.OperationResult/Error/ErrorType.cs
+++ Core/Tools/VIAEventsAssociation.Core.Tools.OperationResult/Error/ErrorType.cs
@@ -2,14 +2,15 @@ namespace VIAEventsAssociation.Core.Tools.OperationResult.Error;
 
 public class ErrorType : Enumeration.Enumeration
 {
-    public static readonly ErrorType UnknownType = new(0, "Unknown");
-    public static readonly ErrorType ValidationFailedType = new(100, "Validation Failed");
-    public static readonly ErrorType ResourceNotFoundType = new(101, "Resource Not Found");
-    public static readonly ErrorType UnauthorizedType = new(102, "Unauthorized");
+    public static readonly ErrorType Unknown = new(0, "Unknown");
+    public static readonly ErrorType ValidationFailed = new(100, "Validation Failed");
+    public static readonly ErrorType ResourceNotFound = new(101, "Resource Not Found");
+    public static readonly ErrorType Unauthorized = new(102, "Unauthorized");
     public static readonly ErrorType InvalidTitle = new(200, "Invalid Title");
     public static readonly ErrorType InvalidDescription = new(202, "Invalid Description");
     public static readonly ErrorType InvalidFromTo = new(203, "Invalid FromTo");
     public static readonly ErrorType InvalidMaxGuests = new(204, "Invalid MaxGuests");
+    public static readonly ErrorType InvalidStatus = new(205, "InvalidStatus");
     public static readonly ErrorType ActionNotAllowed = new(300, "Action Not Allowed");
     
     private ErrorType(int code, string type) : base(code, type) {}
